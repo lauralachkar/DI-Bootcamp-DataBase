90be74ec47571926f33fbf19b4f4f69dfba865b2
lauralachkar <99486645+lauralachkar@users.noreply.github.com>
2022-07-27 14:08:33 +0300

    Add files via upload

--- main.sql
+++ main.sql
@@ -1,30 +1,29 @@
-CREATE TABLE items(
- small_desk int,
- large_desk int ,
- fan int
+CREATE TABLE movies(
+movie_id SERIAL,
+movie_title VARCHAR (50) NOT NULL,
+movie_story TEXT,
+actor_playing_id INTEGER,
+PRIMARY KEY (movie_id),
+FOREIGN KEY (actor_playing_id) REFERENCES actors (actor_id)
 );
 
-CREATE TABLE customer(
- FirstName char(4),
- lastname char(4)
+CREATE TABLE producers(
+producer_id SERIAL PRIMARY KEY,
+producer_name text NOT NULL,
+movie_id int,
+FOREIGN KEY(movie_id) REFERENCES movies(movie_id)
 );
 
+INSERT INTO movies (movie_title, movie_story, actor_playing_id) VALUES
+    ( 1,'Good Will Hunting', 
+    'Written by Affleck and Damon, the film follows 20-year-old South Boston janitor Will Hunting');
+INSERT INTO producers (producer_id,producer_name,movie_id) VALUES
+     (1,"Steven Spilberg",1);
 
-INSERT INTO items VALUES (100,300,80);
-INSERT INTO customer VALUES('Greg','Jones');
-INSERT INTO customer VALUES('Sandra','Jones');
-INSERT INTO customer VALUES('Scott','Scott');
-INSERT INTO customer VALUES('Trevor','Green');
-INSERT INTO customer VALUES('Melanie','Johnson');
-SELECT * FROM items;
-SELECT * FROM customer;
-SELECT small_desk FROM items;
-SELECT large_desk FROM items;
+SELECT * from movies;  
+SELECT * from producers;  
 
-SELECT small_desk FROM items;
-SELECT fan FROM items;
-
-
-SELECT * FROM customer WHERE lastname = 'Smith';
-SELECT * FROM customer WHERE lastname = 'Jones';
-SELECT lastname FROM customer WHERE lastname != 'Scott' ;
+SELECT movies.movie_id, movies.movie_title, movies.movie_story,movies.actor_playing_id
+FROM movies
+INNER JOIN producers
+ON movies.movie_id = producers.movie_id;
